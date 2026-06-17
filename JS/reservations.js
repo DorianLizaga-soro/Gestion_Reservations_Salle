7030bb4a873eaf876e4f223e8f36e057a0d920ee
Reinier Lombaard <reinier.lombaard@yahoo.com>
2026-06-17 17:46:40 +0200

    ...

---
+++ JS/reservations.js
@@ -0,0 +1,13 @@
+/* add the Modal et le input group into the JS file and link to the HTML...
+
+        TODO - change and verify the different needs :
+        2x buttons
+        2x dropdowns
+        1x dateInput
+        2x timestamps (Start and finish)
+        1x Description (textbox/commentaire box)
+        1x upload doccument (PDF)
+        1x commentaire box (50car max)
+        2x buttons (close modal + submit)
+
+*/
