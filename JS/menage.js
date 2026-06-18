04ea9a154fa61e28930ddad13dee25ede12e23f2
Dorian Lizaga-soro <dorianlizagasoro@gmail.com>
2026-06-18 17:29:37 +0200

    frontend

--- JS/menage.js
+++ JS/menage.js
@@ -6,3 +6,10 @@ const divCommentaire = document.querySelector(".partie_commentaire");
 btn.addEventListener("click", () => {
     divCommentaire.classList.toggle("active");
 });
+
+function deconnexion(){
+    if (confirm("Souhaitez-vous vous déconnecter ?")){
+        window.location.href = '/html/connexion.html';
+    }
+}
+
