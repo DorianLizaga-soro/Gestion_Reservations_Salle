f57ca008a6ed2ffc3a7984b1eb47a059e4ee1ef4
Reinier Lombaard <reinier.lombaard@yahoo.com>
2026-06-19 11:32:18 +0200

    Friday

--- JS/reservations.js
+++ JS/reservations.js
@@ -11,6 +11,7 @@
         2x buttons (close modal + submit)
 
 */
+const reservations = [];
 
 const dropZone = document.getElementById("dropZone");
 const inputFile = document.getElementById("programmePdf");
@@ -93,3 +94,88 @@ if (dropZone && inputFile) {
     setFileName("Fichier sélectionné : " + file.name);
   });
 }
+
+// store current selected reservation types
+ const buttons = document.querySelectorAll(".reservation-type");
+
+      let reservationType = "ponctuelle";
+
+      buttons.forEach(button => {
+        button.addEventListener("click", () => {
+
+          // Remove selection from all buttons
+          buttons.forEach(btn => btn.classList.remove("active-type"));
+
+          // Select clicked button
+          button.classList.add("active-type");
+
+          // Save current type
+          reservationType =
+            button.id === "btnPonctuelle"
+              ? "ponctuelle"
+              : "recurrente";
+
+          console.log("Selected:", reservationType);
+        });
+      });
+      
+
+      const ponctuelleForm = document.getElementById("ponctuelleForm");
+      const recurrenteForm = document.getElementById("recurrenteForm");
+
+  buttons.forEach(button => {
+  button.addEventListener("click", () => {
+
+    buttons.forEach(btn => btn.classList.remove("active-type"));
+    button.classList.add("active-type");
+
+    if (button.id === "btnPonctuelle") {
+      reservationType = "ponctuelle";
+
+      ponctuelleForm.style.display = "block";
+      recurrenteForm.style.display = "none";
+
+    } else {
+      reservationType = "recurrente";
+
+      ponctuelleForm.style.display = "none";
+      recurrenteForm.style.display = "block";
+    }
+  });
+});
+
+      const reservation = {
+          type: reservationType,
+          association: document.getElementById("Asso_select").value,
+          salle: document.getElementById("salle_select").value,
+          date: document.getElementById("modalDate").value,
+          start: document.getElementById("modalStartTime").value,
+          end: document.getElementById("modalEndTime").value
+      };
+
+          console.log(reservation);
+
+
+          const form = document.getElementById("reservationForm");
+
+          form.addEventListener("submit", function(event) {
+
+              event.preventDefault();
+
+              const date = document.getElementById("date").value;
+              const heure = document.getElementById("heure").value;
+              const nom = document.getElementById("nom").value;
+
+              const tbody = document.getElementById("reservationTableBody");
+
+              const row = document.createElement("tr");
+
+              row.innerHTML = `
+                  <td>${date}</td>
+                  <td>${heure}</td>
+                  <td>${nom}</td>
+              `;
+
+          tbody.appendChild(row);
+
+});
